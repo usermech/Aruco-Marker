bd917cb90889eff7a7ffebc253e2c59efb56ae79
Umut Kurt <96394892+usermech@users.noreply.github.com>
2022-10-30 16:07:53 +0300

    Add files via upload

---
+++ aruco_gen.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL}
+tag_id = 0
+tag_dict = cv2.aruco.Dictionary_get(ARUCO_DICT["DICT_4X4_100"])
+tag = np.zeros((200, 200), dtype=np.uint8)
+tag = cv2.aruco.drawMarker(tag_dict, tag_id, 200, tag, 1)
+cv2.imwrite("tag2.png", tag)
